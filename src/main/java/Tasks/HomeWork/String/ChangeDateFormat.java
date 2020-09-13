c8f2b20774464940d7009555c69cd5d28bfb3c31
Dmitry Shimansky <shimansky.dmitry@gmail.com>
2020-09-13 14:23:30 +0300

    Reworked

--- src/main/java/Tasks/HomeWork/String/ChangeDateFormat.java
+++ src/main/java/Tasks/HomeWork/String/ChangeDateFormat.java
@@ -1,5 +1,7 @@
 package main.java.Tasks.HomeWork.String;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.text.*;
 
@@ -7,20 +9,16 @@ public class ChangeDateFormat {
 
     //написать метод, который принимает на вход строку с датой и временем в формате "22.00 07.09.2020" и выводит в консоль в формате "September, 7, 2020 22:00"
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
         String date = "22.00 07.09.2020";
 
-        SimpleDateFormat ft = new SimpleDateFormat ("E yyyy.MM.dd 'и время' hh:mm:ss a zzz");
-        String str = args.length == 0 ? date : args[0];
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH.mm dd.MM.yyyy");
+        LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
 
-        System.out.print("Строка " + str + " распаршена как ");
-        Date parsingDate;
-        try {
-            parsingDate = ft.parse(str);
-            System.out.println(parsingDate);
-        }catch (ParseException e) {
-            System.out.println("Нераспаршена с помощью " + ft);
-        }
+        DateTimeFormatter newFormatter = DateTimeFormatter.ofPattern("MMMM, d, yyyy HH:mm ");
+
+        System.out.println(dateTime.format(newFormatter));
+
     }
 }
